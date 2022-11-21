a23de242f30971b8c9e84d4be2f9652a96c1d461
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-11-21 09:28:21 +0900

    set timeout for connect

--- cmd/connect.go
+++ cmd/connect.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
+
+	"github.com/Songmu/timeout"
 )
 
 func connect() {
 	configIfNotExist()
 
 	cmd := "/opt/cisco/anyconnect/bin/vpn -s < " + c.configPath
-	out, err := exec.Command("sh", "-c", cmd).Output()
+
+	tio := &timeout.Timeout{
+		Cmd:       exec.Command("sh", "-c", cmd),
+		Duration:  5 * time.Second,
+		KillAfter: 5 * time.Second,
+	}
+
+	exitStatus, out, _, err := tio.Run()
+	if exitStatus.Signaled {
+		fmt.Println("ERROR : Timeout is occurred. Please reconfiguration your setting by using `any-connect config`.")
+		os.Exit(1)
+	}
+
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
