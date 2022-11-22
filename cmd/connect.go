b450934e203a356d4371f2e9cc6936485ab0b610
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-11-23 02:50:54 +0900

    add timeout

--- cmd/connect.go
+++ cmd/connect.go
@@ -1,13 +1,11 @@
 package cmd
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"os"
 	"os/exec"
+	"syscall"
 	"time"
-
-	"github.com/Songmu/timeout"
 )
 
 func showPS() {
@@ -19,103 +17,30 @@ func connect() {
 	configIfNotExist()
 
 	cmdstr := "/opt/cisco/anyconnect/bin/vpn -s < " + c.configPath
-
-	// tio := &timeout.Timeout{
-	// 	Cmd:       exec.Command("sh", "-c", cmd),
-	// 	Duration:  10 * time.Second,
-	// 	KillAfter: 5 * time.Second,
-	// }
-
-	// exitStatus, out, _, err := tio.Run()
-	// if exitStatus.Signaled {
-	// 	fmt.Println(string(out))
-	// 	fmt.Println("ERROR : Timeout is occurred. Please reconfiguration your setting by using `any-connect config`.")
-	// 	os.Exit(1)
-	// }
-
 	cmd := exec.Command("sh", "-c", cmdstr)
 
-	// out, err := exec.Command("sh", "-c", cmdstr).Output()
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	os.Exit(1)
-	// }
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
-	// var stdout bytes.Buffer
-	// var stderr bytes.Buffer
-	// cmd.Stdout = &stdout
-	// cmd.Stderr = &stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
 
-	stdout, _ := cmd.StdoutPipe()
-	// stdout, err := cmd.StdoutPipe()
-	// if err != nil {
-	// 	fmt.Println("cmd.StdoutPipe() error: " + err.Error())
-	// }
+	cmd.Start()
+
 	go func() {
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			line := scanner.Text()
-			fmt.Println(line)
-		}
+		t := time.NewTimer(3 * time.Second)
+		<-t.C
+		fmt.Println("process kill")
+		syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+		fmt.Println("pid : ", cmd.Process.Pid)
+		fmt.Println("ERROR : Timeout is occurred. Please reconfiguration your setting by using `any-connect config`.")
 	}()
 
-	tio := &timeout.Timeout{
-		Cmd:       cmd,
-		Duration:  10 * time.Second,
-		KillAfter: 5 * time.Second,
+	cmd.Wait()
+
+	if cmd.Err != nil {
+		fmt.Println(string(stderr.Bytes()))
 	}
-	exitStatus, out, _, err := tio.Run()
-	if exitStatus.Signaled {
-		fmt.Println(string(out))
-		fmt.Println("ERROR : Timeout is occurred. Please reconfiguration your setting by using `any-connect config`.")
-		os.Exit(1)
-	}
-	if err != nil {
-		fmt.Println(string(out))
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	// err := cmd.Run()
-	// get pipe to standard output
-	//stdout, err := cmd.StdoutPipe()
-	// if err != nil {
-	// 	fmt.Println("cmd.StdoutPipe() error: " + err.Error())
-	// }
-
-	// cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	// cmd.Start()
-	// fmt.Println("process start", cmd.Process.Pid)
-	// time.Sleep(time.Second)
-	// showPS()
-	// if err != nil {
-	// 	fmt.Printf("Stdout: %s\n", stdout.String())
-	// 	fmt.Printf("Stderr: %s\n", stderr.String())
-	// } else {
-	// 	fmt.Printf("Stdout: %s\n", stdout.String())
-	// }
-	// t := time.NewTimer(5 * time.Second)
-	// <-t.C
-	// syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
-	// fmt.Println(cmd.Process.Pid)
-	// fmt.Println("ERROR : Timeout is occurred. Please reconfiguration your setting by using `any-connect config`.")
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	os.Exit(1)
-	// }
-
-	// go func() {
-	// 	err := cmd.Wait()
-	// 	fmt.Println("process done:", err)
-	// }()
-
-	// t := time.NewTimer(5 * time.Second)
-	// <-t.C
-	// fmt.Println("process kill")
-
-	// syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
-	// time.Sleep(time.Second)
-	//fmt.Println(string(stdout.Bytes()))
-	showPS()
-	fmt.Println(out)
-	//fmt.Println(string(out))
+	fmt.Println(string(stdout.Bytes()))
 }
